23f09215626b316402c9ee1f92dc3a063b49d894
rich.gg <me@rich.gg>
2023-10-12 12:04:04 +0400

    First Commit

---
+++ app/src/main/java/gg/rich/pkotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package gg.rich.pkotlin
+
+import android.os.Bundle
+//import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import processing.android.CompatUtils
+import processing.android.PFragment
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // FrameLayoutを作成
+        val frame = FrameLayout(this)
+        frame.id = CompatUtils.getUniqueViewId()
+        setContentView(
+            frame,
+            ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        )
+        // PAppletクラスを継承したSketchクラスをNewする
+        val sketch = Sketch()
+        // PFragmentで描画
+        val fragment = PFragment(sketch)
+        fragment.setView(frame, this)
+    }
+}
---
+++ app/src/main/java/gg/rich/pkotlin/sketch.kt
@@ -0,0 +1,27 @@
+package gg.rich.pkotlin
+
+import processing.core.PApplet
+class Sketch : PApplet() {
+    override fun setup() {
+        // 背景色
+        background(255)
+    }
+    override fun draw() {
+        noStroke()
+        // 色をランダムに設定
+        val r = random(0f, 255f)
+        val g = random(0f, 255f)
+        val b = random(0f, 255f)
+        // 位置をランダムに設定
+        val x = random(0f, width.toFloat())
+        val y = random(0f, height.toFloat())
+        // 円の大きさをランダムに設定
+        val d = random(10f, 100f)
+        // 塗る
+        fill(r, g, b)
+        // 描く
+        stroke(r, g, b)
+        // 円
+        ellipse(x, y, d, d)
+    }
+}
